6287646148ed42efb6f7e3eb3f337f5b93284ed3
Leandro Carrijo <7leandrobatista@gmail.com>
2023-11-27 14:33:54 -0300

    Exception handling - delete

--- src/main/java/com/educanoweb/course/services/UserService.java
+++ src/main/java/com/educanoweb/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.educanoweb.course.repositories.UserRepository;
 import com.educanoweb.course.services.exceptions.DatabaseException;
 import com.educanoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -42,9 +44,13 @@ public class UserService {
 		}
 	}
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
